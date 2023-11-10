6f6d046e9ce3cbe1368246b093336420a941713b
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-11-10 03:55:15 -0400

    first steps with NextJS

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import StyledJsxRegistry from './registry'
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="pt-br">
+        <body> 
+          <StyledJsxRegistry>{children}</StyledJsxRegistry>
+          </body>
+      </html>
+    )
+  }
